38816d7ccc57eabce86ce5181e168918b44f15ea
AzureAD\joshchiang蔣逸凡 <aya11111@msn.com>
2021-09-02 17:37:51 +0800

    init

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>後台系統登入</title>
+        <style>
+            body {
+                margin: 0;
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div style="display: flex;">
+            <div style="flex-basis: 70%; height: 100vh; max-width: 70%; background-image: url('/images/login/voyager-assets.jpg'); background-size: cover;">
+            </div>
+            <div style="flex-basis: 30%; max-width: 30%;">
+                <div style="color: #f00">開始寫登入</div>
+            </div>
+        </div>
+    </body>
+</html>
